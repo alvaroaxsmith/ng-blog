d3c287267f075ee908a99c2edadbe8d5cb025232
alvaroun <alvaromachadoferreira@hotmail.com>
2022-04-01 18:30:59 -0300

    create feed component

--- frontend/blog/src/app/app.module.ts
+++ frontend/blog/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { FeedComponent } from './feed/feed.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    FeedComponent
   ],
   imports: [
     BrowserModule,
