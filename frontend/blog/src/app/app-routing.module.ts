34ae4302e63d4fa17f992e49319d6accc8255b25
alvaroun <alvaromachadoferreira@hotmail.com>
2022-04-01 18:42:59 -0300

    create routing

--- frontend/blog/src/app/app-routing.module.ts
+++ frontend/blog/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { FeedComponent } from './feed/feed.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'feed', component: FeedComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
