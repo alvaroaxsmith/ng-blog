23b63acd91dccf2ec057bf6346dead0e69db2a58
alvaroun <alvaromachadoferreira@hotmail.com>
2022-04-01 18:18:23 -0300

    create navbar component

--- frontend/blog/src/app/app.module.ts
+++ frontend/blog/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
