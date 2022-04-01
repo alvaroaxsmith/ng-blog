c6e5334928dae43ee34ad4c9f2c0d3faf81a6af0
alvaroun <alvaromachadoferreira@hotmail.com>
2022-04-01 19:09:55 -0300

    add font awsome & bootstrap

--- frontend/blog/src/app/app.module.ts
+++ frontend/blog/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { FeedComponent } from './feed/feed.component';
 import { ContatoComponent } from './contato/contato.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { ContatoComponent } from './contato/contato.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
