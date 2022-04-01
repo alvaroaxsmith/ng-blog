475148b70fe2d5d04a07f2ac74938565cef75f2b
alvaroun <alvaromachadoferreira@hotmail.com>
2022-04-01 18:47:46 -0300

    update routing

--- frontend/blog/src/app/app-routing.module.ts
+++ frontend/blog/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { ContatoComponent } from './contato/contato.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
@@ -7,7 +8,8 @@ import { HomeComponent } from './home/home.component';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
-    { path: 'feed', component: FeedComponent }
+    { path: 'feed', component: FeedComponent },
+    { path: 'contato', component: ContatoComponent}
 ];
 
 @NgModule({
