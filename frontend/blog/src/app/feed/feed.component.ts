a32c244ac26ca65601c5a3409023ad6814dbdfb9
alvaroun <alvaromachadoferreira@hotmail.com>
2022-04-03 22:52:00 -0300

    update feed component

--- frontend/blog/src/app/feed/feed.component.ts
+++ frontend/blog/src/app/feed/feed.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { PostService } from '../service/post.service';
+import { Post } from '../model/Post';
 @Component({
   selector: 'app-feed',
   templateUrl: './feed.component.html',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedComponent implements OnInit {
 
-  constructor() { }
+  listPost!: Post[];
+
+  constructor(private postService: PostService) { }
 
   ngOnInit(): void {
+    this.findPosts()
+  }
+
+  findPosts() {
+    this.postService.getPosts().subscribe((data: Post[])=>{
+      this.listPost = data;
+    })
   }
 
 }
